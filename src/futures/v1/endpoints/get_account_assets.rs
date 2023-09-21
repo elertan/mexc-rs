89ad4bd6f32fcc0992f6128fb8438855b41f7eb6
Dennis Kievits <denkievits@gmail.com>
2023-09-21 02:27:52 +0200

    futures

--- src/futures/v1/endpoints/get_account_assets.rs
+++ src/futures/v1/endpoints/get_account_assets.rs
@@ -1,30 +1,34 @@
 use async_trait::async_trait;
-use chrono::{DateTime, TimeZone, Utc};
-use reqwest::Client;
-use crate::futures::{MexcFuturesApiClient, MexcFuturesApiClientWithAuthentication, MexcFuturesApiEndpoint};
+use bigdecimal::BigDecimal;
+use crate::futures::{MexcFuturesApiClientWithAuthentication};
 use crate::futures::response::ApiResponse;
 use crate::futures::result::ApiResult;
 
+#[derive(Debug, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AccountAsset {
+    pub currency: String,
+    pub position_margin: BigDecimal,
+    pub frozen_balance: BigDecimal,
+    pub available_balance: BigDecimal,
+    pub cash_balance: BigDecimal,
+    pub equity: BigDecimal,
+    pub unrealized: BigDecimal,
+}
+
 #[async_trait]
 pub trait GetAccountAssets {
-    async fn get_account_assets(&self) -> ApiResult<DateTime<Utc>>;
+    async fn get_account_assets(&self) -> ApiResult<Vec<AccountAsset>>;
 }
 
 
 #[async_trait]
 impl GetAccountAssets for MexcFuturesApiClientWithAuthentication {
-    async fn get_account_assets(&self) -> ApiResult<DateTime<Utc>> {
+    async fn get_account_assets(&self) -> ApiResult<Vec<AccountAsset>> {
         let url = format!("{}/api/v1/private/account/assets", self.endpoint.as_ref());
         let auth_header_map = self.get_auth_header_map(&())?;
         let response = self.reqwest_client.get(&url).headers(auth_header_map).send().await?;
-        let json = response.text().await?;
-        tracing::debug!("{}", json);
-
-        // let api_response = response.json::<ApiResponse<i64>>().await?;
-        // let timestamp = api_response.into_api_result()?;
-
-
-        todo!();
-        // Ok(Utc.timestamp_millis_opt(timestamp).unwrap())
+        let api_response = response.json::<ApiResponse<Vec<AccountAsset>>>().await?;
+        api_response.into_api_result()
     }
 }
